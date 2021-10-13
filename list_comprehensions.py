1c093ea317b85b1ac19394f168e7ab3901611ce6
Max Landeros <maxlanderos1409@gmail>
2021-10-13 00:06:54 -0500

    Simplificamos nuestra lista con un list comprehension

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -1,10 +1,12 @@
 def run():
-    squeres = []
-    for i in range(1,100):
-        if i % 3 != 0:
-            squeres.append(i**2)
+    # squeres = []
+    # for i in range(1,100):
+    #     if i % 3 != 0:
+    #         squeres.append(i**2)
+    # print(squeres)
+    squeres = [i**2 for i in range(1,101) if i % 3 != 0]
     print(squeres)
 
-
+    
 if __name__=="__main__":
     run()
