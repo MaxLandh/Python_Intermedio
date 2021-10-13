5d4906bfbed439ab9a8e195515452418732be948
Max Landeros <maxlanderos1409@gmail>
2021-10-13 00:21:57 -0500

    Modificamos el list comprehension en numeros que sean multiplos de 4, 6 y 9 a la vez

--- list_comprehensions.py
+++ list_comprehensions.py
@@ -4,7 +4,9 @@ def run():
     #     if i % 3 != 0:
     #         squeres.append(i**2)
     # print(squeres)
-    squeres = [i**2 for i in range(1,101) if i % 3 != 0]
+    # squeres = [i**2 for i in range(1,101) if i % 3 != 0]
+    
+    squeres = [i for i in range(1, 100000) if (i % 4 == 0 and i % 6 == 0 and i % 9 == 0)]
     print(squeres)
 
     
