ae8b0bf10207c80414845775002a6f164179c705
Max Landeros <maxlanderos1409@gmail.com>
2021-10-14 23:45:27 -0500

    Usamos funciones de orden superior y usamos landa

---
+++ filtrando_datos.py
@@ -0,0 +1,96 @@
+##UTF-8
+
+#Datos de empleados, edad, organizacion, posicion y lenguje que dominan
+DATA = [
+    {
+        'name': 'Facundo',
+        'age': 72,
+        'organization': 'Platzi',
+        'position': 'Technical Coach',
+        'language': 'python',
+    },
+    {
+        'name': 'Luisana',
+        'age': 33,
+        'organization': 'Globant',
+        'position': 'UX Designer',
+        'language': 'javascript',
+    },
+    {
+        'name': 'HÃ©ctor',
+        'age': 19,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'ruby',
+    },
+    {
+        'name': 'Gabriel',
+        'age': 20,
+        'organization': 'Platzi',
+        'position': 'Associate',
+        'language': 'javascript',
+    },
+    {
+        'name': 'Isabella',
+        'age': 30,
+        'organization': 'Platzi',
+        'position': 'QA Manager',
+        'language': 'java',
+    },
+    {
+        'name': 'Karo',
+        'age': 23,
+        'organization': 'Everis',
+        'position': 'Backend Developer',
+        'language': 'python',
+    },
+    {
+        'name': 'Ariel',
+        'age': 32,
+        'organization': 'Rappi',
+        'position': 'Support',
+        'language': '',
+    },
+    {
+        'name': 'Juan',
+        'age': 17,
+        'organization': '',
+        'position': 'Student',
+        'language': 'go',
+    },
+    {
+        'name': 'Pablo',
+        'age': 32,
+        'organization': 'Master',
+        'position': 'Human Resources Manager',
+        'language': 'python',
+    },
+    {
+        'name': 'Lorena',
+        'age': 56,
+        'organization': 'Python Organization',
+        'position': 'Language Maker',
+        'language': 'python',
+    },
+]
+
+def run():
+    #Qeremos todos los desarrolladores que manejen Python
+    all_python_devs = [worker["name"] for worker in DATA if worker["language"]=="python"]
+    
+    #Queremos a todos los trabajadores de platzi
+    all_platzi_workers = [worker["name"] for worker in DATA if worker["organization"] == "Platzi"]
+
+    #Queremos a todos los adultos, osea mayores de 18 
+    all_olders = [worker["name"] for worker in DATA if worker["age"] > 18]
+    
+    #Usando las funciones de orden superior
+    adults = list(filter(lambda worker: worker["age"] > 18, DATA))
+    adults = list(map(lambda worker: worker["name"], adults))
+    old_people = list(map(lambda worker: worker | {"old": worker["age"] > 70}, DATA))
+    for old in old_people:
+        print(old)   
+
+
+if __name__=="__main__":
+    run()   
